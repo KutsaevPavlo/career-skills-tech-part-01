236a00e10dd33994df0a93e40e63a74dc47675f8
KutsaevPavlo <p1ul@ukr.net>
2023-05-09 00:06:14 +0300

    css styled cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 // import { Wrapper, Avatar, Logo, Info, Box, Thumb } from "./Card.styled";
+import css from "./Card.module.css";
 
 const Card = ({ item }) => {
   const { user, tweets, followers, avatar } = item;
   return (
-    <div>
-      <img src={avatar} alt={user} />
+    <div className={css.Wrapper}>
+      <img className={css.Logo} src={avatar} alt={user} />
       <p>{tweets} tweets</p>
       <p>{followers} followers</p>
       <button>FOLLOW</button>
